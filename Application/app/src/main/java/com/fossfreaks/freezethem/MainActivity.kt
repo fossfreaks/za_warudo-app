01700574f524bec99f67c416c8ae0c04979d2a29
MageshBabu K <mage1k99@gmail.com>
2020-07-08 01:03:01 +0530

    added additional libs and TODOs

--- Application/app/src/main/java/com/fossfreaks/freezethem/MainActivity.kt
+++ Application/app/src/main/java/com/fossfreaks/freezethem/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // TODO : Show onBoarding Scree
+        // TODO : Check for root permissions
+        // TODO : Use package Manager for getting list OF packages
+        // TODO : Disable them with for loop
+        // TODO : Show if they are disabled
+        // TODO : ENABLE as user clicks unFreeze
+
     }
 }
