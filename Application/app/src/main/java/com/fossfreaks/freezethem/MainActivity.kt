3d36396ef68177446a1e8521ede59bfb98cfd495
MageshBabu K <mage1k99@gmail.com>
2020-07-13 18:21:56 +0530

    inital try for root

--- Application/app/src/main/java/com/fossfreaks/freezethem/MainActivity.kt
+++ Application/app/src/main/java/com/fossfreaks/freezethem/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.fossfreaks.freezethem
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.jaredrummler.android.shell.Shell
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +20,26 @@ class MainActivity : AppCompatActivity() {
         // TODO : Disable them with for loop
         // TODO : Show if they are disabled
         // TODO : ENABLE as user clicks unFreeze
+        test_btn.setOnClickListener {
+            getAllApps()
+        }
+    }
 
+    private fun getAllApps(){
+        val packageManager = packageManager
+        val packages = packageManager.getInstalledPackages(PackageManager.GET_META_DATA)
+        test_btn.text = "Package Name -- Application Info -- Version Name"
+        packages.forEach {
+            test_btn.text = "${test_btn.text} \n ${it.packageName} -- ${it.applicationInfo} ${it.versionName}"
+        }
+    }
+
+    private fun getRootPermissions():Boolean{
+        var isRooted = false
+        GlobalScope.launch {
+            val result = Shell.SU.run("ls -a")
+            isRooted = result.isSuccessful
+        }
+        return isRooted
     }
 }
