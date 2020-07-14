5477188e858dca9014a1f0a65a205cbf7f9d21cf
MageshBabu K <mage1k99@gmail.com>
2020-07-14 12:47:05 +0530

    added alertDialogHelper

--- Application/app/src/main/java/com/fossfreaks/freezethem/callbacks/DelDogCallbacks.kt
+++ Application/app/src/main/java/com/fossfreaks/freezethem/callbacks/DelDogCallbacks.kt
@@ -1,4 +1,11 @@
 package com.fossfreaks.freezethem.callbacks
 
+import android.content.ClipboardManager
+
 interface DelDogCallbacks {
+    /*
+    * @prams uploadCrashLogs
+    * This method is used to uploadCrashLogs
+    * */
+    fun uploadCrashLogs(message:String,clipboardManager: ClipboardManager)
 }
--- Application/app/src/main/java/com/fossfreaks/freezethem/helper/AlertDialogHelper.kt
+++ Application/app/src/main/java/com/fossfreaks/freezethem/helper/AlertDialogHelper.kt
@@ -1,4 +1,41 @@
 package com.fossfreaks.freezethem.helper
 
-class AlertDialogHelper {
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.fossfreaks.freezethem.R
+
+class AlertDialogHelper(private val context: Context) {
+    fun displayExceptionAlert(title: String,message : String,clipboardManager: ClipboardManager,exception: Exception){
+        AlertDialog.Builder(context)
+            .setTitle(title)
+            .setMessage(message)
+            .setIcon(R.drawable.ic_baseline_error_outline_24)
+            .setPositiveButton("Okay, I'll Try later") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .setNegativeButton("Copy the Logs!"){ dialog, _ ->
+                val clipData = ClipData.newPlainText("errorLog","$exception")
+                clipboardManager.setPrimaryClip(clipData)
+                Toast.makeText(context, "Copied Logs!", Toast.LENGTH_SHORT).show()
+                dialog.dismiss()
+            }
+            .setNeutralButton("Copy CrashLog Url"){dialog, _ ->
+                //TODO : Callback for uploading and copy to clipboard
+                dialog.dismiss()
+            }
+            .show()
+    }
+    fun displayAlertDialog(title: String,message: String){
+        AlertDialog.Builder(context)
+            .setTitle(title)
+            .setMessage(message)
+            .setIcon(R.drawable.ic_baseline_warning_24)
+            .setPositiveButton("Okay!"){dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
 }
