e997eba110331549df6458cd74aa9e243b79709e
MageshBabu K <mage1k99@gmail.com>
2020-10-15 17:16:03 +0530

    Added KTX Support

--- Freezeem/app/src/main/java/com/fossfreaks/freezeem/ui/MainActivity.kt
+++ Freezeem/app/src/main/java/com/fossfreaks/freezeem/ui/MainActivity.kt
@@ -3,10 +3,13 @@ package com.fossfreaks.freezeem.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.fossfreaks.freezeem.R
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
